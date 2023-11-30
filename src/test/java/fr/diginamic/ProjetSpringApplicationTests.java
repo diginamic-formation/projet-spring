52f801090339c3fb59dcd9ad06203b2813ca1891
Jawouaahhh <back2thedata@gmail.com>
2023-11-30 10:35:43 +0100

    Setup Project

---
+++ src/test/java/fr/diginamic/ProjetSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
